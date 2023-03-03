846f303ac6ce118c0627634bbdce5b9c78c88c9d
Luis-Villalta <118476422+Luis-Villalta@users.noreply.github.com>
2023-03-03 14:30:23 -0400

    Arreglos

--- js/main.js
+++ js/main.js
@@ -15,15 +15,15 @@ function promedio (nota1, nota2, nota3){
 }
 
 function aop (promedio) { 
-{if(promedio > 7)   {
+if(promedio > 7)   {
                     return "Estás aprobado, haz superado mi quiz"
                     }
         else {return "No haz superado mi quiz:("}
-}
+
 
 let rta = "";
 
-while(rta ==""){
+while(rta =="")
 nota1 = Number (prompt("Ingresa tú primera nota"))
 nota2 = Number (prompt("Ingresa tú segunda nota"))
 nota3 = Number (prompt("Ingresa tú tercera nota"))
@@ -31,8 +31,8 @@ nota3 = Number (prompt("Ingresa tú tercera nota"))
 let resultado = promedio(nota1,nota2,nota3)
 console.log (aop(resultado))
 
-rta = prompt("Ingrese \"ESC\" para finalizar tú operación.")}
-}
+rta = prompt("Ingrese \"ESC\" para finalizar tú operación.")
+
 
 /* Para el Proyecto quiero hacer un carrito de compras, me imagino que en el curso dirán cómo. Pero tengo una duda
 el carrito de compras que tipo de función llevaría? */ 
