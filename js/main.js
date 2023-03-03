54fcaec84fa9040ca9b905cddcc8f2c65e2ece75
Luis-Villalta <118476422+Luis-Villalta@users.noreply.github.com>
2023-03-03 15:15:01 -0400

    Arreglos Primera Entrega

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ console.log ("Es un honor tenerte aqui, presiona en CLICK AQUI")
 
 
 
-/* Ayuda no puedo activar la Simulación de notas:( */
-
 function promedio (nota1, nota2, nota3){
 
     return(nota1+nota2+nota3)
@@ -19,11 +17,12 @@ if(promedio > 7)   {
                     return "Estás aprobado, haz superado mi quiz"
                     }
         else {return "No haz superado mi quiz:("}
+}
 
 
 let rta = "";
 
-while(rta =="")
+while(rta =="") {
 nota1 = Number (prompt("Ingresa tú primera nota"))
 nota2 = Number (prompt("Ingresa tú segunda nota"))
 nota3 = Number (prompt("Ingresa tú tercera nota"))
@@ -31,12 +30,12 @@ nota3 = Number (prompt("Ingresa tú tercera nota"))
 let resultado = promedio(nota1,nota2,nota3)
 console.log (aop(resultado))
 
-rta = prompt("Ingrese \"ESC\" para finalizar tú operación.")
+rta = prompt("Ingrese \"ESC\" para finalizar tú operación.") 
+}
 
 
 /* Para el Proyecto quiero hacer un carrito de compras, me imagino que en el curso dirán cómo. Pero tengo una duda
 el carrito de compras que tipo de función llevaría? */ 
 
-/* No logré hacer el simulador de notas , necesito que en la entrega me digas que está mal y porqué... Sería de gran ayuda */
 
 
